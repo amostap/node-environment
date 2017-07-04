0d32fd5ec47c9ab15d1888b5a6ad0647aff068e0
Alexandr Ostapenko <alexandr.amostap@gmail.com>
2017-07-04 16:47:31 +0300

    added production script. chunks, and dinamic html generator

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -7,7 +7,7 @@ import chalk from 'chalk';
 const json = JSON.stringify(jsf(schema));
 
 fs.writeFile("./src/api/db.json", json, function (err) {
-  if(err) {
+  if (err) {
     return console.log(chalk.red(err));
   } else {
     console.log(chalk.green("Mock data generated"));
